5a8be0dab685e883e82a17ab9f15c8f6fb08a47d
rebel216 <neerajbhardwajpkt@gmail.com>
2022-09-22 12:47:48 +0530

    Add required changes.

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,12 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { configureStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import booksReducer from './books/books';
+import Categoriesreducer from './categories/categories';
 
 const reducer = combineReducers({
-  booksReducer,
+  booksReducer, Categoriesreducer,
 });
 
-const store = createStore(reducer, applyMiddleware(logger));
+const store = configureStore(reducer, applyMiddleware(logger));
 
 export default store;
