b9397dc95c877bfc63e3cb935601933292f3f017
rebel216 <neerajbhardwajpkt@gmail.com>
2022-09-23 16:32:02 +0530

    Fixed Linters error

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,6 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import booksReducer from './books/books';
+/* eslint-disable import/extensions */
 import Categoriesreducer from './categories/categories';
 
 const rootreducer = combineReducers({
