0b7af4e95dd3248e4650b04283022e1706c2c52f
rebel216 <neerajbhardwajpkt@gmail.com>
2022-09-24 18:05:14 +0530

    add required changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Books from './components/Books';
+// eslint-disable-next-line import/extensions
 import Categories from './components/categories';
 import Header from './components/Header';
 
